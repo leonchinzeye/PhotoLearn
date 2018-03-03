da5cdad814c51c4c6cd36ae6d04ab28cd9d4c674
Karen Athaide <karenathaide@gmail.com>
2018-03-03 20:31:27 +0800

    Added separate activity folder for traceability

---
+++ app/src/main/java/com/mtech/parttimeone/photolearn/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mtech.parttimeone.photolearn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mtech.parttimeone.photolearn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
