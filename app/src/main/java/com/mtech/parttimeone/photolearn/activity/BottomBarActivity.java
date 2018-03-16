d7ea3415e308435871e7694aef075d33f8c639ef
ChangLing MAC_02 <liu_changling90@163.com>
2018-03-16 17:28:31 +0800

    Add BotttomNavigationbar

---
+++ app/src/main/java/com/mtech/parttimeone/photolearn/activity/BottomBarActivity.java
@@ -0,0 +1,44 @@
+package com.mtech.parttimeone.photolearn.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.mtech.parttimeone.photolearn.R;
+
+public class BottomBarActivity extends BaseActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_main:
+                    mTextMessage.setText(R.string.title_main);
+                    return true;
+                case R.id.navigation_me:
+                    mTextMessage.setText(R.string.title_me);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottombar);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_main);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
