34b294fd5bd30444f2674323d62c5a3a8baec11e
ChangLing <liu_changling90@163.com>
2018-03-17 19:43:31 +0800

    Add logic function for fragment and bottom navigation bar.

--- app/src/main/java/com/mtech/parttimeone/photolearn/activity/BottomBarActivity.java
+++ app/src/main/java/com/mtech/parttimeone/photolearn/activity/BottomBarActivity.java
@@ -1,44 +1,85 @@
 package com.mtech.parttimeone.photolearn.activity;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.mtech.parttimeone.photolearn.R;
+import com.mtech.parttimeone.photolearn.fragments.MeFragment;
 
 public class BottomBarActivity extends BaseActivity {
 
-    private TextView mTextMessage;
+    Fragment meFragment;
+    Fragment mainFragment;
+    Fragment currentFragment;
+    private FragmentManager mFragmentManager;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_main:
-                    mTextMessage.setText(R.string.title_main);
+                    setCurrentFragment(mainFragment);
                     return true;
                 case R.id.navigation_me:
-                    mTextMessage.setText(R.string.title_me);
+                    setCurrentFragment(meFragment);
                     return true;
             }
             return false;
         }
     };
 
+    private void setDefaultFragment(){
+
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+              fragmentTransaction.add(R.id.fragment_content, mainFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        currentFragment = mainFragment;
+    }
+
+    // INIT the UI
+    private void intheView(){
+        mFragmentManager = getSupportFragmentManager();
+        meFragment = new MeFragment();
+        mainFragment = new Fragment();
+    }
+
+    private void setCurrentFragment(Fragment fragment){
+        mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction1 = mFragmentManager.beginTransaction();
+         if (fragment.isAdded()) {
+                    fragmentTransaction1.hide(currentFragment).show(fragment);
+                } else {
+
+                    fragmentTransaction1.hide(currentFragment).add(R.id.fragment_content, fragment);
+                    fragmentTransaction1.addToBackStack(null);
+
+                }
+         currentFragment = fragment;
+         fragmentTransaction1.commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottombar);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.navigation_main);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        intheView();
+        setDefaultFragment();
     }
 
 }
---
+++ app/src/main/java/com/mtech/parttimeone/photolearn/fragments/MeFragment.java
@@ -0,0 +1,110 @@
+package com.mtech.parttimeone.photolearn.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.mtech.parttimeone.photolearn.R;
+
+public class MeFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+   // private OnFragmentInteractionListener mListener;
+   String[] infolist = {"Google", "Apple"};
+   private ListView listView;
+
+    public MeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_me, container, false);
+        listView = (ListView) view.findViewById(R.id.me_list);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, infolist){
+
+            //set row height.
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the current item from ListView
+                View view = super.getView(position,convertView,parent);
+
+                // Get the Layout Parameters for ListView Current Item View
+                ViewGroup.LayoutParams params = view.getLayoutParams();
+
+                // Set the height of the Item View
+                params.height = 150;
+                view.setLayoutParams(params);
+
+                return view;
+            }
+        };
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+}
